bdcd7c60100ef5a9c1ac3a833b7f6e5cb39f89c2
Sharath44665 <sharathwelcomesyouin@gmail.com>
2024-12-03 18:37:16 +0530

    Passing JSX as children

--- 06PassingProps/src/App.jsx
+++ 06PassingProps/src/App.jsx
@@ -1,39 +1,28 @@
-import { getImageUrl } from "./utils";
+import Avatar from './Avatar.jsx';
+import "./App.css";
 
-
-function Avatar( {person, size}){
+function Card({ children }) {
   return (
-    <img src= {getImageUrl (person)}
-        alt={person.name}
-        width={size}
-        height={size}
-    />
+    <div className="card">
+      {children}
+    </div>
   );
 }
 
-export default function Profile(){
+export default function Profile() {
   return (
-    <div>
-      <Avatar size={100}
-        person={{
-          name: 'Katsuko Saruhashi', 
+    <Card>
+      <Avatar
+        size={100}
+        person={{ 
+          name: 'Katsuko Saruhashi',
           imageId: 'YfeOqp2'
         }}
       />
-      <Avatar
-        size={80}
-        person={{
-          name: 'Aklilu Lemma', 
-          imageId: 'OKS67lh'
-        }}
-      />
-      <Avatar
-        size={50}
-        person={{ 
-          name: 'Lin Lanying',
-          imageId: '1bX5QH6'
-        }}
-      />
-    </div>
-  )
+    </Card>
+  );
 }
+
+
+
+
--- 06PassingProps/src/utils.js
+++ 06PassingProps/src/utils.js
@@ -1,8 +1,9 @@
-export function getImageUrl(person, size = 's'){
-    return ( 
-        'https://i.imgur.com/' +
-        person.imageId +
-        size +
-        '.jpg'
+export function getImageUrl(person, size = 's') {
+    return (
+      'https://i.imgur.com/' +
+      person.imageId +
+      size +
+      '.jpg'
     );
-}
+  }
+  
