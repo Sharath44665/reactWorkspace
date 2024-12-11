2236f9e31d891188f72b0aad6368f79f7e529c3b
Sharath44665 <sharathwelcomesyouin@gmail.com>
2024-12-11 23:20:59 +0530

    conditional rendering exercise

---
+++ 10CondnRenderEx/src/App.jsx
@@ -0,0 +1,36 @@
+function Item({ name, importance }) {
+
+  return (
+    <li className="item">
+      {name}{` (importance: ${importance} )`}
+    </li>
+  )
+  
+  // return (
+  //   <li className="item">
+  //     {name} {importance>0 && "(importance: "+importance +")"}
+  //   </li>
+  // );
+}
+
+export default function PackingList() {
+  return (
+    <section>
+      <h1>Sally Ride's Packing List</h1>
+      <ul>
+        <Item 
+          importance={9} 
+          name="Space suit" 
+        />
+        <Item 
+          importance={0} 
+          name="Helmet with a golden leaf" 
+        />
+        <Item 
+          importance={6} 
+          name="Photo of Tam" 
+        />
+      </ul>
+    </section>
+  );
+}
