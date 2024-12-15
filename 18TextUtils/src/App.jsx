b838259351edc77a5c625235b520b38860dc0068
Sharath44665 <sharathwelcomesyouin@gmail.com>
2024-12-15 15:07:32 +0530

    add clear button

--- 18TextUtils/src/App.jsx
+++ 18TextUtils/src/App.jsx
@@ -1,5 +1,6 @@
 // Fix a broken clock - clock.js
 
+import About from "./components/About";
 import Navbar from "./components/Navbar";
 import TextForm from "./components/TextForm";
 
@@ -9,8 +10,11 @@ function App(){
     <>
       <Navbar title="Text Utils" aboutText="about_text_utils" />
       <div className="container">
-        <TextForm heading="Enter ur text to analyze" />
+        {/* <TextForm heading="Enter ur text to analyze" /> */}
+        <About /> 
       </div>
+
+      
       
 
     </>
--- 18TextUtils/src/components/TextForm.jsx
+++ 18TextUtils/src/components/TextForm.jsx
@@ -13,16 +13,47 @@ export default function TextForm(props){
         setText(event.target.value)
     }
 
+    const handleLowerClick = () => {
+        const newText = text.toLowerCase();
+        setText(newText)
+    }
+
+    const handleClearClick = () => {
+        setText("")
+    }
 
     const [text, setText] = useState("enter text here");
+
+    const getWords = (text) => {
+        let wordsCounter = text.split(" ").length;
+        
+        if (text.endsWith(" ") || text == ""){
+            return wordsCounter - 1;
+        }
+        return wordsCounter;
+    }
     return (
+        <>
         <div>
             <h2>{props.heading} </h2>
             <div className="mb-3">
                 <textarea className="form-control" value={text} onChange={handleOnChange} id="mybox" rows="8"></textarea>
             </div>
 
-            <button className="btn btn-primary" onClick={handleUpperClick}  >Convert to UPPERCASE</button>
+            <button className="btn btn-primary mx-2" onClick={handleUpperClick}  >Convert to UPPERCASE</button>
+
+            <button className="btn btn-primary mx-2" onClick={handleLowerClick}  >Convert to lowercase</button>
+            <button type="button" className="btn btn-secondary mx-2" onClick={handleClearClick} >clear</button>
         </div>
+        <div className="container">
+            <h2>your text summary:</h2>
+            <p>{getWords(text)} words, {text.length} charecters</p>
+            <p>{0.008 * getWords(text) } minutes to read</p>
+
+            <h2>Preview</h2>
+            <p>{text}</p>
+        </div>
+        </>
+        
     )
 }
