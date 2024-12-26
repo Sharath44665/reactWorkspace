d4cb6fba0f94f4d21b14432179a8441ba8b1700d
Sharath44665 <sharathwelcomesyouin@gmail.com>
2024-12-26 22:31:34 +0530

    add db

---
+++ 20inotebook/backend/db.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+const mongoURI = "mongodb://localhost:27017/";
+const connectToMongo = () => {
+
+    mongoose.connect(mongoURI)
+        .then(() => console.log('Connected to MongoDB...'))
+        .catch((error) => console.error('Connection error', error));
+}
+
+
+module.exports = connectToMongo;
+
