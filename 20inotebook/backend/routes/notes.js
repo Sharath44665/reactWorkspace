ff060f9c96369b25a867012c8e3123554004afe4
Sharath44665 <sharathwelcomesyouin@gmail.com>
2025-01-03 22:48:01 +0530

    updatte notes

--- 20inotebook/backend/routes/notes.js
+++ 20inotebook/backend/routes/notes.js
@@ -17,6 +17,7 @@ router.get('/all', fetchuser, async (req, res) => {
     }
 })
 
+// POST create note
 router.post('/addnote', fetchuser, [
     body('title').isLength({ min: 3 }).withMessage("minimum 3 charecters required"),
     body('description').isLength({ min: 5 }).withMessage("minimum password length is 5"),
@@ -42,4 +43,39 @@ router.post('/addnote', fetchuser, [
     }
 })
 
+
+// PUT update notes
+
+router.put('/update/:id', fetchuser, async (req, res) => {
+    const {title, description, tag} = req.body;
+    let newNote = {}
+
+    if ( title ) { newNote.title = title}
+    if ( description ) { newNote.description = description }
+    if ( tag ) { newNote.tag = tag }
+
+    console.log(newNote);
+    
+    // console.log(req.params.id)
+    let note = await Notes.findById(req.params.id) // id coming from '/update:id'
+    
+    // notes doesnot exist
+    if(!note) {
+        return res.status(404).json({error: "notes not found"})
+    }
+    
+    // if user doesnot match with notes.user 
+    if ( note.user.toString() !== req.userFound.id ){
+        return res.status(401).json({errorMsg : 'not allowed'})
+    }
+
+
+    // note = await Notes.findByIdAndUpdate( req.params.id, { $set: newNote }, { new:true } )
+    console.log('--------------')
+    console.log(newNote)
+    note = await Notes.findByIdAndUpdate( req.params.id, newNote, { new:true } )
+
+    res.json({msg: 'update success.' , data:note})
+
+} )
 module.exports = router
