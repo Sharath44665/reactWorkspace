11f665349c30ffb913d8f681562d55de82a520cb
Sharath44665 <sharathwelcomesyouin@gmail.com>
2025-01-03 17:07:42 +0530

    initial setup for notes, returns empty notes

--- 20inotebook/backend/model/Notes.js
+++ 20inotebook/backend/model/Notes.js
@@ -1,4 +1,6 @@
-import mongoose from 'mongoose';
+// import mongoose from 'mongoose';
+
+const mongoose = require('mongoose')
 const { Schema } = mongoose;
 
 const NotesSchema = new Schema({
--- 20inotebook/backend/routes/notes.js
+++ 20inotebook/backend/routes/notes.js
@@ -1,10 +1,13 @@
 const express = require('express');
+const fetchuser = require('../middleware/fetchUser');
+const Notes = require('../model/Notes.js')
 const router = express.Router();
 
-
-router.get('/', (req, res) => {
+// all notes GET
+router.get('/all', fetchuser, async (req, res) => {
+    const notes = await Notes.find({user:req.userFound.id})
     
-    res.json([])
+    res.json({msg: 'success', data: [notes]})
 })
 
 module.exports = router
