72e5794282c26b7376c7556b204c38df700d3d9a
Sharath44665 <sharathwelcomesyouin@gmail.com>
2025-01-04 21:09:53 +0530

    useContext hook: using react context API

--- 20inotebook/src/components/About.jsx
+++ 20inotebook/src/components/About.jsx
@@ -1,10 +1,15 @@
-import React, { useContext } from 'react';
+import React, { useContext, useEffect } from 'react';
 import noteContext from '../context/notes/NoteContext';
 
 function About() {
   const a = useContext(noteContext)
+  useEffect(() => {
+      a.update()
+      // eslint-disable-next-line
+    }, [])
+
   return (
-    <div>this is About {a.name} and he is in {a.class}</div>
+    <div>this is About {a.state.name} and he is in {a.state.class}</div>
   )
 }
 
--- 20inotebook/src/context/notes/NoteState.jsx
+++ 20inotebook/src/context/notes/NoteState.jsx
@@ -1,12 +1,24 @@
+import { useState } from "react";
 import NoteContext from "./NoteContext";
 
 const NoteState = (props) => {
-    const state = {
+    const s1 = {
         "name": "sharath",
-        "class" : "7a"
+        "class": "7a"
+    }
+
+    const [state, setState] = useState(s1)
+
+    const update = () => {
+        setTimeout(() => {
+            setState({
+                "name": "dem user",
+                "class": "10b"
+            })
+        }, 1000);
     }
     return (
-        <NoteContext.Provider value={state} >
+        <NoteContext.Provider value={{state, update}} >
             {props.children}
         </NoteContext.Provider>
     )
