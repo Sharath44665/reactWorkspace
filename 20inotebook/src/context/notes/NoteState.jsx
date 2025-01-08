1ce0384e01a41eb6540f3f24b10013e0eeb64a12
sharath44665 <sharathwelcomesyouin@gmail.com>
2025-01-08 16:28:11 +0530

    delete from web and mogodb

--- 20inotebook/src/context/notes/NoteState.jsx
+++ 20inotebook/src/context/notes/NoteState.jsx
@@ -24,8 +24,20 @@ const NoteState = (props) => {
 
    
 
-    const addNote =(title, description, tag) =>{
+    const addNote = async (title, description, tag) =>{
         // TODO: API Call
+        const response = await fetch(`${host}/api/notes/all`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'auth-token': 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyRm91bmQiOnsiaWQiOiI2NzdkMTZjNDY0NWRlYmI4MzU0MTFhMjcifSwiaWF0IjoxNzM2MzIwNjUyfQ.9sVV9FBZgowTcG7YRuEnA_VVcraoo3g2MmXOzIAPmTc'
+
+            }, 
+
+            body: JSON.stringify({title, description, tag}) // i think, this taking inputs from text box
+        });
+
+        console.log('adding a new note')
         const note = {
             "_id": "67d7b9498bc66cd668f5e288a",
             "user": "6777e08b593ad2450851ce49",
@@ -39,13 +51,50 @@ const NoteState = (props) => {
 
     }
 
-    const editNote =(id, title, description, tag) =>{
-        
+    const editNote = async (id, title, description, tag) =>{
+        const response = await fetch(`${host}/api/notes/all`, {
+            method: 'PUT',
+            headers: {
+                'Content-Type': 'application/json',
+                'auth-token': 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyRm91bmQiOnsiaWQiOiI2NzdkMTZjNDY0NWRlYmI4MzU0MTFhMjcifSwiaWF0IjoxNzM2MzIwNjUyfQ.9sVV9FBZgowTcG7YRuEnA_VVcraoo3g2MmXOzIAPmTc'
+
+            }, 
+
+            body: JSON.stringify({title, description, tag}) // i think, this taking inputs from text box
+        });
+
+        const json = response.json();
+
+        console.log(`json edit resposne: ${json}`)
+        console.log(`notes json edit resposne: ${notes}`)
+
+        for ( let idx = 0; idx< notes.length; idx += 1){
+            const element = notes[idx]
+            if (element._id === id ){
+
+                element.title = title;
+                element.description = description;
+                element.tag = tag;
+            }
+        }
     }
     
-    const deleteNote =(id) =>{
+    const deleteNote = async(id) =>{
         // TODO: API Call
-        console.log(`your id: ${id}`)
+        const response = await fetch(`${host}/api/notes/deletenote/${id}`, {
+            method: 'DELETE',
+            headers: {
+                'Content-Type': 'application/json',
+                'auth-token': 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyRm91bmQiOnsiaWQiOiI2NzdkMTZjNDY0NWRlYmI4MzU0MTFhMjcifSwiaWF0IjoxNzM2MzIwNjUyfQ.9sVV9FBZgowTcG7YRuEnA_VVcraoo3g2MmXOzIAPmTc'
+
+            }, 
+
+            
+        });
+        
+        const json = response.json();
+        console.log('delete note from id: ---')
+        console.log(json)
         const newNotes = notes.filter( (note) =>  note._id  !==  id ) // return everything except the id selected for deleting
         setNotes(newNotes)
         
