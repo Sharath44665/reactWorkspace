0f23cdd99d7582fcbbe88f09eb058b62965b03d0
sharath44665 <sharathwelcomesyouin@gmail.com>
2025-01-08 17:26:17 +0530

    update note: show updated object in console

--- 20inotebook/src/components/Notes.jsx
+++ 20inotebook/src/components/Notes.jsx
@@ -6,22 +6,32 @@ import NoteContext from '../context/notes/NoteContext.jsx';
 
 function Notes() {
     const context = useContext(NoteContext);
-    const { notes, getNotes } = context
+    const { notes, getNotes } = context;
+    const [note, setNote] = useState({ etitle: "", edescription: "", etag: "default" });
 
     useEffect(() => {
         getNotes()
         // eslint-disable-next-line
     }, [])
 
-    const updateNote = (note) => {
+    
+
+    const updateNote = (currentNote) => {
         ref.current.click()
+        setNote({
+            etitle:currentNote.title, 
+            edescription: currentNote.description,
+            etag: currentNote.tag
+        })
     }
 
     const ref = useRef(null)
 
-    const [note, setNote] = useState({ title: "", description: "", tag: "default" });
+    
 
     const handleClick = (e) => {
+        console.log('update note---')
+        console.log(note)
         e.preventDefault();
         
         
@@ -52,23 +62,23 @@ function Notes() {
                             <form>
                                 <div className="mb-3">
                                     <label htmlFor="etitle" className="form-label"  >Title</label>
-                                    <input type="text" className="form-control" id="etitle" name="etitle" aria-describedby="emailHelp" onChange={onChange} />
+                                    <input type="text" className="form-control" id="etitle" name="etitle" aria-describedby="emailHelp" onChange={onChange} value={note.etitle} />
 
                                 </div>
                                 <div className="mb-3">
                                     <label htmlFor="edescription" className="form-label">Description</label>
-                                    <input type="text" className="form-control" id="edescription" name="edescription" onChange={onChange} />
+                                    <input type="text" className="form-control" id="edescription" name="edescription" onChange={onChange} value={note.edescription} />
                                 </div>
                                 <div className="mb-3">
                                     <label htmlFor="etag" className="form-label">Tag</label>
-                                    <input type="text" className="form-control" id="etag" name="etag" onChange={onChange} placeholder="optional" />
+                                    <input type="text" className="form-control" id="etag" name="etag" onChange={onChange} placeholder="optional" value={note.etag} />
                                 </div>
                                 
                             </form>
                         </div>
                         <div className="modal-footer">
                             <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-                            <button type="button" className="btn btn-primary">Update</button>
+                            <button type="button" className="btn btn-primary" onClick={handleClick}>Update</button>
                         </div>
                     </div>
                 </div>
